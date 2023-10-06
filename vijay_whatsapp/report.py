e4e9421c3f936ab43062838b94d55cc3dc8a6852
Pithiya-Nilesh <pithiyanilesh87@gmail.com>
2023-10-06 11:56:19 +0530

    a

--- vijay_whatsapp/report.py
+++ vijay_whatsapp/report.py
@@ -8,7 +8,9 @@ import json
 @frappe.whitelist(allow_guest=True)
 # def get_report_content(report="", report_type='', filters={'company': 'Vijay Mamra Private Limited', 'ageing_based_on': 'Due Date', 'range1': '30', 'range2': '60', 'range3': '90', 'range4': '120'}, data_modified_till=""):
 def get_receivable_report_content(name):
-    filters = "{'company': 'Vijay Mamra Private Limited', 'ageing_based_on': 'Due Date', 'range1': '30', 'range2': '60', 'range3': '90', 'range4': '120', 'party_type': 'Customer','party': f'[{name}]'}"
+    # filters = "{'company': 'Vijay Mamra Private Limited', 'ageing_based_on': 'Due Date', 'range1': '30', 'range2': '60', 'range3': '90', 'range4': '120', 'party_type': 'Customer','party': f'[{name}]'}"
+
+    filters = {'company': 'Vijay Mamra Private Limited', 'ageing_based_on': 'Due Date', 'range1': '30', 'range2': '60', 'range3': '90', 'range4': '120', 'party_type': 'Customer','party': f'["name"]'}
 
     """Returns file in for the report in given format"""
 
